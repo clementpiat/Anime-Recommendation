cb8e4da76ac2de3f3e668ad338c75139a0484e14
QuentinBlampey <quentin.blampey@gmail.com>
2021-04-09 11:14:00 +0200

    fix merge

--- personalized_page_rank.py
+++ personalized_page_rank.py
@@ -8,23 +8,31 @@ import networkx as nx
 import argparse
 from fast_pagerank import pagerank_power
 import numpy as np
+from utils import get_bipartite_graph
 
-from test import test
+from test import testing
 
-def recommend_ppr(adj_matrix, nodes, user_index, K=5):
+class PPR:
+    def __init__(self, K=5):
+        self.adj_matrix, self.nodes = get_bipartite_graph(as_scipy=True)
+        self.nodes_to_index = dict([(node,i) for i,node in enumerate(self.nodes)])
+        self.K = K
 
-    personalize = np.zeros(adj_matrix.shape[0])
-    personalize[user_index] = 1
-    ppr = pagerank_power(adj_matrix, p=0.6, personalize=personalize, tol=1e-6)
-    couples = sorted(enumerate(ppr), key=lambda x: 0 if adj_matrix[user_index, x[0]] or nodes[x[0]][0]=="u" else x[1])[-K:]
-    return list(map(lambda x: int(nodes[x[0]][2:]), couples))
-    
-    # ppr = nx.pagerank_scipy(G, alpha=0.7, personalization={f"u_{user_id}": 1})
-    # couples = sorted(ppr.items(), key=lambda x: 0 if G.has_edge(x[0], f"u_{user_id}") or x[0][0]=="u" else x[1])[-K:]
-    # return list(map(lambda x: int(x[0][2:]), couples))
+    def __call__(self, user_id):
+        user_index = self.nodes_to_index[f"u_{user_id}"]
+
+        personalize = np.zeros(self.adj_matrix.shape[0])
+        personalize[user_index] = 1
+        ppr = pagerank_power(self.adj_matrix, p=0.6, personalize=personalize, tol=1e-6)
+        couples = sorted(enumerate(ppr), key=lambda x: 0 if self.adj_matrix[user_index, x[0]] or self.nodes[x[0]][0]=="u" else x[1])[-self.K:]
+        return list(map(lambda x: int(self.nodes[x[0]][2:]), couples))
+        
+        # ppr = nx.pagerank_scipy(G, alpha=0.7, personalization={f"u_{user_id}": 1})
+        # couples = sorted(ppr.items(), key=lambda x: 0 if G.has_edge(x[0], f"u_{user_id}") or x[0][0]=="u" else x[1])[-K:]
+        # return list(map(lambda x: int(x[0][2:]), couples))
 
 def main(args):
-    test(recommend_ppr, n_users=args.n_users)
+    testing(PPR(), n_users=args.n_users)
 
     
 
--- test.py
+++ test.py
@@ -2,25 +2,21 @@ from tqdm import tqdm
 import json
 from collections import Counter
 
-from utils import get_bipartite_graph, get_test
+from utils import get_test
 
-def test(recommender, n_users=10):
-    adj_matrix, nodes = get_bipartite_graph(as_scipy=True)
-    nodes_to_index = dict([(node,i) for i,node in enumerate(nodes)])
-    test = get_test()
-
+def testing(recommender, n_users=10):
     total_recommendations = []
-    good_recommendations, recommendations, watched, n_users = 0,0,0,0
+    good_recommendations, recommendations, watched = 0,0,0
+
+    test = get_test()
     for user_id, anime_ids in tqdm(list(test.items())[:n_users]):
         try:
-            user_index = nodes_to_index[f"u_{user_id}"]
-            recommended_anime_ids = recommender(adj_matrix, nodes, user_index)
+            recommended_anime_ids = recommender(user_id)
 
             total_recommendations.extend(recommended_anime_ids)
             good_recommendations += len(set(recommended_anime_ids).intersection(anime_ids))
             recommendations += len(recommended_anime_ids)
             watched += len(anime_ids)
-            n_users += 1
         except:
             # Unknown user
             pass
